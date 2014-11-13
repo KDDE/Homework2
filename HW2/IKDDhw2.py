d55a56056dba0376f159711927d42b9be74bc023
Stanley Ding <stanleyding.tw@gmail.com>
2014-11-13 10:09:32 +0800

    add some table-drawing functions

--- HW2/IKDDhw2.py
+++ HW2/IKDDhw2.py
@@ -5,6 +5,8 @@ host = 'host=iServDB.cloudopenlab.org.tw port=5432'
 dbname = 'dbname=stanley811213_db_7744'
 login_info = 'user=stanley811213_user_7744 password=OUvWLGmm'
 
+COL_WIDTH = [40, 20, 15]
+
 def char_width(char):
     width = unicodedata.east_asian_width(unicode(char))
     if width == 'Na':
@@ -28,18 +30,30 @@ def partition(string, n):
 
     return [string[:i] + ' ' * (n - width)] + partition(string[i:], n)
 
-def print_row(data):
-    text = partition(data[1], 30)
-    author = partition(data[2], 20)
-    twitter_id = partition(data[3], 15)
+def print_line():
+    for width in COL_WIDTH:
+        print '+' + '-' * width
+    print '+'
 
-    print('+' + '-' * 30 + '+' + '-' * 20 + '+' + '-' * 15 + '+')
+def print_row(data):
+    text = partition(data[1], COL_WIDTH[0])
+    author = partition(data[2], COL_WIDTH[1])
+    twitter_id = partition(data[3], COL_WIDTH[2])
+
+    print_line()
     lines = max(len(text), len(author), len(twitter_id))
     for i in range(lines):
-        _text = text[i] if len(text) > i else ' ' * 30
-        _author = author[i] if len(author) > i else ' ' * 20
-        _twitter_id = twitter_id[i] if len(twitter_id) > i else ' ' * 15
+        _text = text[i] if len(text) > i else ' ' * COL_WIDTH[0]
+        _author = author[i] if len(author) > i else ' ' * COL_WIDTH[1]
+        _twitter_id = twitter_id[i] if len(twitter_id) > i else ' ' * COL_WIDTH[2]
         print '|' + _text + '|' + _author + '|' + _twitter_id + '|'
+
+def print_header():
+    print_line()
+    print '|text' + ' ' * (COL_WIDTH[0] - len('text')),
+    print '|user_name' + ' ' * (COL_WIDTH[1] - len('user_name')),
+    print '|user_id' + ' ' * (COL_WIDTH[2] - len('user_id'))
+    print '|'
     
 conn = psycopg2.connect(host + ' ' + dbname + ' ' + login_info)
 cur = conn.cursor()
@@ -49,7 +63,7 @@ cur.execute("SELECT * FROM twitter WHERE q='" + query + "';")
 result = cur.fetchall()
 
 final_result = sorted(result, key=lambda x: int(x[3]))
-print final_result[0]
+
 for i in final_result:
     print_row(i)
 
