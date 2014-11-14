b63d86c68bdb51fd2775a54beadd13fc020a13bb
Stanley Ding <stanleyding.tw@gmail.com>
2014-11-14 09:42:24 +0800

    add table module.

--- HW2/IKDDhw2.py
+++ HW2/IKDDhw2.py
@@ -1,71 +1,22 @@
 import psycopg2
 import unicodedata
 
+import table
+
 host = 'host=iServDB.cloudopenlab.org.tw port=5432'
 dbname = 'dbname=stanley811213_db_7744'
 login_info = 'user=stanley811213_user_7744 password=OUvWLGmm'
 
-COL_WIDTH = [40, 20, 15]
-
-def char_width(char):
-    width = unicodedata.east_asian_width(unicode(char))
-    if width == 'Na':
-        return 1
-    else:
-        return 2
-
-def partition(string, n):
-    if string == '':
-        return []
-
-    width = 0
-    for i, c in enumerate(string):
-        width += char_width(c)
-        if width > n:
-            break
-    if width <= n:
-        i += 1
-    else:
-        width -= char_width(c)
-
-    return [string[:i] + ' ' * (n - width)] + partition(string[i:], n)
-
-def print_line():
-    for width in COL_WIDTH:
-        print '+' + '-' * width
-    print '+'
-
-def print_row(data):
-    text = partition(data[1], COL_WIDTH[0])
-    author = partition(data[2], COL_WIDTH[1])
-    twitter_id = partition(data[3], COL_WIDTH[2])
-
-    print_line()
-    lines = max(len(text), len(author), len(twitter_id))
-    for i in range(lines):
-        _text = text[i] if len(text) > i else ' ' * COL_WIDTH[0]
-        _author = author[i] if len(author) > i else ' ' * COL_WIDTH[1]
-        _twitter_id = twitter_id[i] if len(twitter_id) > i else ' ' * COL_WIDTH[2]
-        print '|' + _text + '|' + _author + '|' + _twitter_id + '|'
-
-def print_header():
-    print_line()
-    print '|text' + ' ' * (COL_WIDTH[0] - len('text')),
-    print '|user_name' + ' ' * (COL_WIDTH[1] - len('user_name')),
-    print '|user_id' + ' ' * (COL_WIDTH[2] - len('user_id'))
-    print '|'
-    
 conn = psycopg2.connect(host + ' ' + dbname + ' ' + login_info)
 cur = conn.cursor()
 
 query = raw_input('Input: ')
-cur.execute("SELECT * FROM twitter WHERE q='" + query + "';")
+cur.execute("SELECT text, user_name, user_id  FROM twitter WHERE q='" + query + "';")
 result = cur.fetchall()
 
-final_result = sorted(result, key=lambda x: int(x[3]))
+final_result = sorted(result, key=lambda x: int(x[2]))
 
-for i in final_result:
-    print_row(i)
+table.print_table(final_result)
 
 cur.close()
 conn.close()
---
+++ HW2/table.py
@@ -0,0 +1,65 @@
+import unicodedata
+
+COL_WIDTH = [40, 20, 15]
+
+def pad_string(string, width):
+    n = string_width(string)
+    padding = 0 if n >= width else width - n
+    return string + ' ' * padding
+
+def char_width(char):
+    width = unicodedata.east_asian_width(char)
+    if width == 'Na':
+        return 1
+    else:
+        return 2
+
+def string_width(string):
+    width = 0
+    for char in string:
+        width += char_width(char)
+    return width
+
+def partition(string, n):
+    if string == '':
+        return []
+
+    width = 0
+    for i, c in enumerate(string):
+        width += char_width(c)
+        if width > n or c == '\n':
+            break
+    if width <= n:
+        i += 1
+    return [string[:i]] + partition(string[i:], n)
+
+def print_line():
+    for width in COL_WIDTH:
+        print '+' + '-' * width,
+    print '+'
+
+def print_row(tweet_data):
+    tweet_data = [x.decode('utf8') for x in tweet_data]
+    for i in range(len(tweet_data)):
+        tweet_data[i] = partition(tweet_data[i], COL_WIDTH[i])
+
+    print_line()
+    lines = len(max(tweet_data, key=lambda x: len(x)))
+    for i in range(lines):
+        for n, col in enumerate(tweet_data):
+            s = col[i] if len(col) > i else ''
+            print '|' + pad_string(s, COL_WIDTH[n]),
+        print '|'
+
+def print_header():
+    print_line()
+    print '|text' + ' ' * (COL_WIDTH[0] - len('text')),
+    print '|user_name' + ' ' * (COL_WIDTH[1] - len('user_name')),
+    print '|user_id' + ' ' * (COL_WIDTH[2] - len('user_id')),
+    print '|'
+
+def print_table(data):
+    print_header()
+    for i in data:
+        print_row(i)
+    print_line()
